66b61ef00bcab6d654c0cbfcb380e037f1935a26
pops <pops@DESKTOP-P3QPI08>
2025-07-02 16:41:54 +0300

    added produser and consumer for text

---
+++ ConsoleTransitionsDataByLAN.Consumer/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Configuration;
+using System.Net;
+using System.Net.Sockets;
+using System.Reflection.PortableExecutable;
+
+namespace ConsoleTransitionsDataByLAN.Consumer
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            //var text = "value1";
+            //var ms= new MemoryStream();
+            //var binWriter = new BinaryWriter(ms);
+            //binWriter.Write(text);
+            //binWriter.Seek(0, SeekOrigin.Begin);
+
+
+            //var st = ms.ToArray();
+
+            //BinaryReader reader = new BinaryReader(new MemoryStream(st));
+            //byte[] result = reader.ReadBytes((int)binWriter.BaseStream.Length);
+
+            //var str = System.Text.Encoding.Default.GetString(result);
+            //Console.WriteLine(str);
+
+            var port = int.Parse(ConfigurationManager.AppSettings["port"]);
+            var client = new UdpClient(port);
+
+            Console.WriteLine($"{string.Join('\n',Dns.GetHostEntry(Dns.GetHostName()).AddressList.Where(x=>x.AddressFamily==AddressFamily.InterNetwork))}\n" +
+                $"\"press any to continue...\"\n");
+            Console.ReadLine();
+            Console.WriteLine("started listening");
+
+            var receiveText = "";
+            while (true)
+            {
+                var data = await client.ReceiveAsync();
+                receiveText= receiveText==""?System.Text.Encoding.Default.GetString(data.Buffer):receiveText;
+                Console.WriteLine(receiveText);
+                Console.Title = $"bytes received: {data.Buffer.Length * sizeof(byte)}";
+            }
+        }
+    }
+}
--- ConsoleTransitionsDataByLAN.Producer/Program.cs
+++ ConsoleTransitionsDataByLAN.Producer/Program.cs
@@ -1,10 +1,38 @@
-﻿namespace ConsoleTransitionsDataByLAN.Producer
+﻿using System.Configuration;
+using System.Net;
+using System.Net.Sockets;
+
+namespace ConsoleTransitionsDataByLAN.Producer
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var udpClient = new UdpClient();
+            var sendText = "Hello world!";
+
+            var consumerIp = ConfigurationManager.AppSettings["consumerIp"];
+            var consumerPort = int.Parse(ConfigurationManager.AppSettings["consumerPort"]);
+
+            var consumerEndPoint = new IPEndPoint(IPAddress.Parse(consumerIp), consumerPort);
+
+            Console.WriteLine($"consumer: {consumerEndPoint}");
+            while (true)
+            {
+                try
+                {
+                    var ms = new MemoryStream();
+                    var binWriter = new BinaryWriter(ms);
+                    binWriter.Write(sendText);
+                    binWriter.Seek(0, SeekOrigin.Begin);
+
+                    udpClient.Send(ms.ToArray(), (int)ms.Length, consumerEndPoint);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex);
+                }
+            }
         }
     }
 }
